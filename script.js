5bc45f308a2c8dc16d38faf8e0628e2ae3bad616
GouravSoni9068 <gouravsoni9068@gmail.com>
2024-04-13 16:29:04 +0530

    font page with cursor and nav animation done

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+let crsr=document.querySelector(".cursor");
+
+document.addEventListener("mousemove",(info)=>{
+    console.log(info.x);
+    console.log(info.y);
+    
+    crsr.style.top=info.y+"px";
+    crsr.style.left=info.x+"px";
+})
+
+let blur=document.querySelector(".cursor-blur");
+
+document.addEventListener("mousemove",(info)=>{
+    console.log(info.x);
+    console.log(info.y);
+    
+    blur.style.top=info.y-150+"px";
+    blur.style.left=info.x-150+"px";
+})
+
+
 gsap.to("#nav",{
     backgroundColor:"black",
     height:" 45px",
@@ -8,4 +29,18 @@ gsap.to("#nav",{
         end:"top -15%",
         scrub:1,
     }
+})
+
+
+gsap.to("#main",{
+    backgroundColor:"black",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        // markers:true,
+        start:"top1 -25%",
+        end:"top -70%",
+        scrub:3,
+    }
+
 })
