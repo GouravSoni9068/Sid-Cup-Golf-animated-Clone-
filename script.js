c3fe4d1165ea0b9119f4a18d8d8f43f9960de36c
GouravSoni9068 <gouravsoni9068@gmail.com>
2024-04-24 01:22:41 +0530

    make footer and nav hover effect and animation between cards and a line in page4

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let crsr=document.querySelector(".cursor");
 
 document.addEventListener("mousemove",(info)=>{
-    console.log(info.x);
-    console.log(info.y);
+    // console.log(info.x);
+    // console.log(info.y);
     
     crsr.style.top=info.y+"px";
     crsr.style.left=info.x+"px";
@@ -11,12 +11,28 @@ document.addEventListener("mousemove",(info)=>{
 let blur=document.querySelector(".cursor-blur");
 
 document.addEventListener("mousemove",(info)=>{
-    console.log(info.x);
-    console.log(info.y);
+    // console.log(info.x);
+    // console.log(info.y);
     
     blur.style.top=info.y-200+"px";
     blur.style.left=info.x-200+"px";
 })
+
+let allNavLi= document.querySelectorAll("#nav ul li");
+allNavLi.forEach(ele => {
+    ele.addEventListener("mouseenter",()=>{
+        crsr.style.scale="2";
+        crsr.style.border="solid 2px white"
+        crsr.style.backgroundColor="transparent";
+    })
+    ele.addEventListener("mouseleave",()=>{
+        crsr.style.scale="1";
+        crsr.style.border="none"
+        crsr.style.backgroundColor=" #95C11E";
+    })
+});
+
+
 
 
 gsap.to("#nav",{
@@ -101,4 +117,39 @@ gsap.from(".rightQuote",{
         scrub:3,
     }
     
+})
+
+// let card1=document.querySelector(".page4card1");
+// let card2=document.querySelector(".page4card2");
+// let card3=document.querySelector(".page4card3");
+
+
+// card1.addEventListener('mouseover',()=>{
+//     card1.style.backgroundImage='url("images/page4card1.webp")';
+//     card1.style.color="white";
+// })
+// card2.addEventListener('mouseover',()=>{
+//     card2.style.backgroundImage='url("images/page4card2.webp")';
+//     card2.style.color="white";
+
+// })
+// card3.addEventListener('mouseover',()=>{
+//     card3.style.backgroundImage='url("images/page4card1.webp")';
+
+//     card3.style.color="white";
+
+// })
+
+
+gsap.from(".page4 h1",{
+    y:43,
+    
+    scrollTrigger:{
+        trigger:".page4 h1",
+        scroller:"body",
+        // markers:true,
+        start:"top 60%",
+        end:"top 55%",
+        scrub:2,
+    }
 })
